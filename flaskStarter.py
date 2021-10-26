69d4bf078e16f8fbe7b4857f4e647859d785233d
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2021-10-26 14:29:26 +0000

    dada

--- flaskStarter.py
+++ flaskStarter.py
@@ -1,12 +1,41 @@
 from flask import Flask, render_template, request
 import os
+from stuff import *
+import camelot
+import numpy as np
+import pandas as pd
+import json
+from stuff import *
+import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './input'
 
 @app.route('/')
-def first_page():
-    return render_template('first_page.html')
+def pt1():
+    # file = "../input/11okt10nov.pdf"
+    file ='/input/11okt10nov.pdf'
+    cellinfo = []
+
+    tables = camelot.read_pdf(file,pages='1-end')
+    cellinfo = tablestocellinfo(tables)
+
+    with open('celldata1.json', 'w') as f:
+        json.dump(cellinfo,f)
+
+    docs = []
+
+    # tables[0].col = tables[0].df.col + 1
+    # tables[0].row = tables[0].df.row + 2
+
+    # tables[1].col = tables[1].df.col + 1
+    # tables[1].row = tables[1].df.row + 1
+
+
+    cleanuptables(tables,docs)
+    return str(docs)
+# def first_page():
+#     return render_template('first_page.html')
 
 @app.route('/response_page', methods = ['POST'])
 def response_page():
@@ -30,5 +59,6 @@ def upload_file():
         
 if __name__ == "__main__":
         port = int(os.environ.get("PORT", 5000))
+        port = process.env.PORT
         app.run(host='0.0.0.0', port=port)
         #app.run()
